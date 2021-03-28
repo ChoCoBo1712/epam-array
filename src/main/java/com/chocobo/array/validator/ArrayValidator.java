d692629bf61f2415083620d780ac16913d57165e
ChoCoBo1712 <ChoCoBo1712@users.noreply.github.com>
2021-03-29 02:08:11 +0300

    ArrayValidator init

--- src/main/java/com/chocobo/array/validator/ArrayValidator.java
+++ src/main/java/com/chocobo/array/validator/ArrayValidator.java
@@ -1,6 +1,8 @@
 package com.chocobo.array.validator;
 
+import java.util.List;
+
 public interface ArrayValidator {
 
-
+    List<Integer> validate(String line);
 }
--- src/main/java/com/chocobo/array/validator/impl/ArrayValidatorImpl.java
+++ src/main/java/com/chocobo/array/validator/impl/ArrayValidatorImpl.java
@@ -2,6 +2,23 @@ package com.chocobo.array.validator.impl;
 
 import com.chocobo.array.validator.ArrayValidator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayValidatorImpl implements ArrayValidator {
 
+    @Override
+    public List<Integer> validate(String line) {
+        List<Integer> numbers = new ArrayList<>();
+        String[] strings = line.split(", ");
+        for (String string : strings) {
+            try {
+                numbers.add(Integer.parseInt(string));
+            } catch (NumberFormatException e) {
+                return new ArrayList<>();
+            }
+        }
+
+        return numbers;
+    }
 }
