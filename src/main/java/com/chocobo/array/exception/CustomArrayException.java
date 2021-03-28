e74fe71bfff83a8d7f55168a45a566e4c210ea0b
ChoCoBo1712 <ChoCoBo1712@users.noreply.github.com>
2021-03-28 16:56:36 +0300

    custom exception init

---
+++ src/main/java/com/chocobo/array/exception/CustomArrayException.java
@@ -0,0 +1,22 @@
+package com.chocobo.array.exception;
+
+public class CustomArrayException extends Exception {
+    public CustomArrayException(String message) {
+        super(message);
+    }
+
+    public CustomArrayException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CustomArrayException(Throwable cause) {
+        super(cause);
+    }
+
+    public CustomArrayException(
+            String message, Throwable cause,
+            boolean enableSuppression, boolean writableStackTrace
+    ) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
