900af2866a6ec9b4092f39d083384cce93644ec9
ChoCoBo1712 <ChoCoBo1712@users.noreply.github.com>
2021-03-29 00:25:08 +0300

    sort tests init

--- src/test/java/com/chocobo/array/service/impl/ArraySortServiceImplTest.java
+++ src/test/java/com/chocobo/array/service/impl/ArraySortServiceImplTest.java
@@ -1,4 +1,45 @@
 package com.chocobo.array.service.impl;
 
+import com.chocobo.array.entity.CustomArray;
+import com.chocobo.array.exception.CustomArrayException;
+import com.chocobo.array.service.ArraySortService;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 public class ArraySortServiceImplTest {
+
+    private CustomArray array;
+    private CustomArray sortedArray;
+    private ArraySortService service;
+
+    @BeforeClass
+    public void setUp() throws CustomArrayException {
+        array = new CustomArray(5);
+        sortedArray = new CustomArray(5);
+        for (int i = 0; i < array.getLength(); i++) {
+            array.setElement(i, array.getLength() - i);
+            sortedArray.setElement(i, i + 1);
+        }
+
+        service = new ArraySortServiceImpl();
+    }
+
+    @Test
+    public void insertionSortTest() throws CustomArrayException {
+        service.insertionSort(array);
+        Assert.assertEquals(array, sortedArray);
+    }
+
+    @Test
+    public void selectionSortTest() throws CustomArrayException {
+        service.selectionSort(array);
+        Assert.assertEquals(array, sortedArray);
+    }
+
+    @Test
+    public void quickSortTest() throws CustomArrayException {
+        service.quickSort(array, 0, array.getLength() - 1);
+        Assert.assertEquals(array, sortedArray);
+    }
 }
