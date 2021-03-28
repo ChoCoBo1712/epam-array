4e2ac26b3933b91ac3a158d87349c7b0da90cdb4
ChoCoBo1712 <ChoCoBo1712@users.noreply.github.com>
2021-03-28 22:32:30 +0300

    quickSort init

--- src/main/java/com/chocobo/array/service/ArraySortService.java
+++ src/main/java/com/chocobo/array/service/ArraySortService.java
@@ -1,4 +1,10 @@
 package com.chocobo.array.service;
 
+import com.chocobo.array.entity.CustomArray;
+import com.chocobo.array.exception.CustomArrayException;
+
 public interface ArraySortService {
+
+    void quickSort(CustomArray array, int begin, int end) throws CustomArrayException;
+
 }
--- src/main/java/com/chocobo/array/service/impl/ArraySortServiceImpl.java
+++ src/main/java/com/chocobo/array/service/impl/ArraySortServiceImpl.java
@@ -1,6 +1,9 @@
 package com.chocobo.array.service.impl;
 
+import com.chocobo.array.entity.CustomArray;
+import com.chocobo.array.exception.CustomArrayException;
 import com.chocobo.array.service.ArraySortService;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -8,5 +11,34 @@ public class ArraySortServiceImpl implements ArraySortService {
 
     private static final Logger logger = LogManager.getLogger();
 
+    @Override
+    public void quickSort(CustomArray array, int begin, int end) throws CustomArrayException {
+        try {
+            if (end <= begin) {
+                return;
+            }
+            int pivot = partition(array, begin, end);
+            quickSort(array, begin, pivot-1);
+            quickSort(array, pivot+1, end);
+        } catch (CustomArrayException e) {
+            logger.log(Level.ERROR, "Index out of bounds");
+            throw e;
+        }
+    }
 
+    private int partition(CustomArray array, int begin, int end) throws CustomArrayException {
+        int counter = begin;
+        for (int i = begin; i < end; i++) {
+            if (array.getElement(i) < array.getElement(end)) {
+                int temp = array.getElement(counter);
+                array.setElement(counter, array.getElement(i));
+                array.setElement(i, temp);
+                counter++;
+            }
+        }
+        int temp = array.getElement(end);
+        array.setElement(end, array.getElement(counter));
+        array.setElement(counter, temp);
+        return counter;
+    }
 }
