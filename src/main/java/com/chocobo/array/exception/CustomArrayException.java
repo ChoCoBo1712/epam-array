c96cb66f0a635fa9e2510b6b93034a280609d10c
ChoCoBo1712 <ChoCoBo1712@users.noreply.github.com>
2021-04-05 13:20:16 +0300

    exception fix

--- src/main/java/com/chocobo/array/exception/CustomArrayException.java
+++ src/main/java/com/chocobo/array/exception/CustomArrayException.java
@@ -1,22 +1,17 @@
 package com.chocobo.array.exception;
 
 public class CustomArrayException extends Exception {
+    public CustomArrayException() { }
+
     public CustomArrayException(String message) {
         super(message);
-    }
-
-    public CustomArrayException(String message, Throwable cause) {
-        super(message, cause);
     }
 
     public CustomArrayException(Throwable cause) {
         super(cause);
     }
 
-    public CustomArrayException(
-            String message, Throwable cause,
-            boolean enableSuppression, boolean writableStackTrace
-    ) {
-        super(message, cause, enableSuppression, writableStackTrace);
+    public CustomArrayException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
